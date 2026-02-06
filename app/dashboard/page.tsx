cd9e73f45178694feb484abce774fb68c99de5f2
gayashan4lk <gayashan4lk@gmail.com>
2026-02-06 01:50:37 -0800

    add shadcn

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,15 @@
+import { Button } from "@/components/ui/button"
 import Link from "next/link"
+
 
 export default function Dashboard() {
   return (
     <div>
       <main>
         <h1 className="text-4xl">Dashboard</h1>
-				<Link href="/">Go back to Home</Link>
+				<Button variant="default">
+					<Link href="/">Go back to Home</Link>
+				</Button>
       </main>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Link from "next/link"
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <main>
         <h1 className="text-4xl">Welcome to PROJEQT</h1>
-        <Link href="/dashboard">Go to Dashboard</Link>
+        <Button variant="default" asChild>
+          <Link href="/dashboard">Go to Dashboard</Link>
+        </Button>
       </main>
     </div>
   );
