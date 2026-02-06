6392b44254f71f149d80672a94a80c28349eac75
gayashan4lk <gayashan4lk@gmail.com>
2026-02-06 10:08:33 -0800

    feat(prisma): implement Prisma client setup and integrate project fetching in Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import prisma from "@/lib/prisma";
 
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const projects = await prisma.project.findMany();
+
   return (
     <div>
       <main>
@@ -10,6 +13,13 @@ export default function Dashboard() {
 				<Button variant="default">
 					<Link href="/">Go back to Home</Link>
 				</Button>
+        <ol>
+          {projects.map((project) => (
+            <li key={project.id}>
+              {project.name} | {project.status} | {project.startDate.toDateString()} - {project.deliveryDate.toDateString()} | {project.description}
+            </li>
+          ))}
+        </ol>
       </main>
     </div>
   );
