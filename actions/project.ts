e386e5f84b6445372874ae333997d816e39fb4d9
gayashan4lk <gayashan4lk@gmail.com>
2026-02-08 14:26:22 +0530

    feat: refactor project form validation and move schema to a dedicated file

--- actions/project.ts
+++ actions/project.ts
@@ -2,21 +2,14 @@
 
 import prisma from '@/lib/prisma'
 import { z } from 'zod'
-import type { ProjectFormData, ProjectActionResponse } from '@/types/project'
-
-const projectSchema = z.object({
-	name: z.string().min(1, 'Name is required'),
-	description: z.string().min(1, 'Description is required'),
-	startDate: z.iso.datetime({ message: 'Invalid start date' }),
-	deliveryDate: z.iso.datetime({ message: 'Invalid delivery date' }),
-	status: z.enum(['not-started', 'on-track', 'off-track', 'on-hold', 'completed']),
-})
+import { projectSchema } from '@/lib/schemas/projectSchema'
+import type { ProjectActionResponse } from '@/lib/schemas/projectSchema'
 
 export async function createProject(
 	prevState: ProjectActionResponse | null,
 	formData: FormData,
 ): Promise<ProjectActionResponse> {
-	const rowFormData = {
+	const rawFormData = {
 		name: formData.get('name') as string,
 		description: formData.get('description') as string,
 		startDate: new Date(formData.get('startDate') as string).toISOString(),
@@ -24,31 +17,27 @@ export async function createProject(
 		status: formData.get('status') as string,
 	}
 
-	console.log('rowFormData', rowFormData)
+	const parsedProduct = projectSchema.safeParse(rawFormData)
 
-	const parseResult = projectSchema.safeParse(rowFormData)
+	const product = parsedProduct.data
 
-	console.log('cleanFormData', parseResult)
-
-	const data = parseResult.data
-
-	if (!parseResult.success) {
+	if (!parsedProduct.success) {
 		return {
 			success: false,
 			message: 'Please fix the form errors',
-			errors: z.flattenError(parseResult.error),
+			errors: z.flattenError(parsedProduct.error),
 		}
 	}
 
-	if (data) {
+	if (product) {
 		try {
 			await prisma.project.create({
 				data: {
-					name: data.name,
-					description: data.description,
-					startDate: data.startDate,
-					deliveryDate: data.deliveryDate,
-					status: data.status,
+					name: product.name,
+					description: product.description,
+					startDate: product.startDate,
+					deliveryDate: product.deliveryDate,
+					status: product.status,
 				},
 			})
 		} catch (error) {
