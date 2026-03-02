84595f130e2491a0b3e0973c99398e2d335b5fac
gayashan4lk <gayashan4lk@gmail.com>
2026-03-03 03:08:23 +0530

    update title

--- e2e/project.spec.ts
+++ e2e/project.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('should be able to create a project', async ({ page }) => {
+test('should create a project', async ({ page }) => {
 	await page.goto('http://localhost:3000/dashboard/projects')
 	await page.getByRole('button', { name: 'New Project' }).click()
 	await page.getByRole('textbox', { name: 'Name' }).click()
